a86fdda997d9f88915824f39e9a848f77160157e
Muhammad Yousuf <84890859+MuhammadYousufbhimla@users.noreply.github.com>
2022-11-25 23:19:44 +0500

    add

--- lib/home.dart
+++ lib/home.dart
@@ -8,51 +8,53 @@ class Home extends StatefulWidget {
 }
 
 class _SignUpState extends State<Home> {
-  bool _validate = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          backgroundColor: Color.fromARGB(255, 20, 95, 130),
-          toolbarHeight: MediaQuery.of(context).size.height * 0.25,
-          title: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                'Muhammad Yousuf',
-                style: TextStyle(
-                  fontSize: MediaQuery.of(context).size.width * 0.06,
-                ),
-              ),
-              Row(
-                children: [
-                  Text(
-                    "Class:BSSE",
-                    style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.03,
-                    ),
+            backgroundColor: Color.fromARGB(255, 20, 95, 130),
+            toolbarHeight: MediaQuery.of(context).size.height * 0.25,
+            title: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  'Muhammad Yousuf',
+                  style: TextStyle(
+                    fontSize: MediaQuery.of(context).size.width * 0.06,
                   ),
-                  Text(
-                    "Roll No:0474",
-                    style: TextStyle(
-                      fontSize: MediaQuery.of(context).size.width * 0.03,
+                ),
+                Padding(
+                    padding: EdgeInsets.only(
+                        top: MediaQuery.of(context).size.height * 0.01)),
+                Row(
+                  children: [
+                    Text(
+                      "Class:BSSE",
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.03,
+                      ),
                     ),
-                  )
-                ],
-              ),
-            ],
-          ),
-          shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(25),
+                    Padding(
+                        padding: EdgeInsets.only(
+                            left: MediaQuery.of(context).size.height * 0.01)),
+                    Text(
+                      "Roll No:0474",
+                      style: TextStyle(
+                        fontSize: MediaQuery.of(context).size.width * 0.03,
+                      ),
+                    )
+                  ],
+                ),
+              ],
             ),
-          ),
-          leading: IconButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              icon: Icon(Icons.arrow_back)),
-        ),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.vertical(
+                bottom: Radius.circular(25),
+              ),
+            ),
+            leading: CircleAvatar(
+              child: Image.asset('assets/login.png'),
+            )),
         body: Container());
   }
 }
